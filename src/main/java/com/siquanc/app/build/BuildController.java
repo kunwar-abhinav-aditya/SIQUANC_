f54e3160e9ce65d46f56b1221758373768d54ade
Kunwar <kunwar.abhinav.aditya@gmail.com>
2018-09-15 15:33:05 +0200

    preflight requests changes

--- src/main/java/com/siquanc/app/build/BuildController.java
+++ src/main/java/com/siquanc/app/build/BuildController.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://frankenstein.sda.tech/build")
 @RestController
 @RequestMapping("/build")
 public class BuildController {

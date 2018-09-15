22335a7394fb8f9a7e0a5a042e7b0390f09b7a5c
Kunwar <kunwar.abhinav.aditya@gmail.com>
2018-09-15 17:40:05 +0200

    CORS

--- src/main/java/com/siquanc/app/build/BuildController.java
+++ src/main/java/com/siquanc/app/build/BuildController.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://frankenstein.sda.tech/build")
 @RestController
 @RequestMapping("/build")
 public class BuildController {
--- src/main/java/com/siquanc/app/query/QueryController.java
+++ src/main/java/com/siquanc/app/query/QueryController.java
@@ -16,6 +16,7 @@ import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 
+@CrossOrigin(origins = "http://frankenstein.sda.tech/query")
 @RestController
 @RequestMapping("/query")
 public class QueryController {
